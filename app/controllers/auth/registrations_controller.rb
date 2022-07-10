de3375ba868c013d83c509b6e8638b7b9044d5f4
伊東和成 <itokazunari5375@icloud.com>
2022-07-10 23:09:40 +0900

    新規作成機能の実装

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -1,7 +1,12 @@
-class Auth::RegistrationsController < ApplicationController
+class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
 
   def sign_up_params
+    puts "========="
+    puts "aaaaa"
+    puts "========="
     params.permit(:name, :email, :password, :image_url, :password_confirmation)
   end
 end
+
+
